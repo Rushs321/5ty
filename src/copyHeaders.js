ee49e5a26456aad15694c0e58f2b831774f051f9
Protoman1234 <rffrgtdgd@gmail.com>
2024-08-14 18:50:27 +0530

    Update copyHeaders.js

--- src/copyHeaders.js
+++ src/copyHeaders.js
@@ -1,11 +1,11 @@
-function copyHeaders(source, target) {
-  for (const [key, value] of Object.entries(source.headers)) {
-    try {
-      target.setHeader(key, value);
-    } catch (e) {
-      console.log(e.message);
+function headerCopy(srcHeaders, tgtResponse) {
+    for (const [headerKey, headerValue] of Object.entries(srcHeaders.headers)) {
+        try {
+            tgtResponse.setHeader(headerKey, headerValue);
+        } catch (err) {
+            console.log(err.message);
+        }
     }
-  }
 }
 
-module.exports = copyHeaders;
+module.exports = headerCopy;
