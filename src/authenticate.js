2986bdde93f4a3af7f87a71399aa0ce03219201f
Protoman1234 <rffrgtdgd@gmail.com>
2024-08-14 18:53:22 +0530

    Update authenticate.js

--- src/authenticate.js
+++ src/authenticate.js
@@ -1,16 +1,16 @@
-const auth = require('basic-auth');
-const LOGIN = process.env.LOGIN;
-const PASSWORD = process.env.PASSWORD;
+const k1 = require('basic-auth');
+const g1 = process.env.LOGIN;
+const g2 = process.env.PASSWORD;
 
-function authenticate(req, res, next) {
-  if (LOGIN && PASSWORD) {
-    const credentials = auth(req);
-    if (!credentials || credentials.name !== LOGIN || credentials.pass !== PASSWORD) {
-      res.setHeader('WWW-Authenticate', `Basic realm="Bandwidth-Hero Compression Service"`);
-      return res.status(401).end('Access denied');
+function xq(req, res, cb) {
+    if (g1 && g2) {
+        const authCredentials = k1(req);
+        if (!authCredentials || authCredentials.name !== g1 || authCredentials.pass !== g2) {
+            res.setHeader('WWW-Authenticate', `Basic realm="Bandwidth-Hero Compression Service"`);
+            return res.status(401).end('Access denied');
+        }
     }
-  }
-  next();
+    cb();
 }
 
-module.exports = authenticate;
+module.exports = xq;
