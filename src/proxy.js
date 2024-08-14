4400a7af58b4c75a0046003cabc933d3e8e8200e
Protoman1234 <rffrgtdgd@gmail.com>
2024-08-14 18:51:33 +0530

    Update proxy.js

--- src/proxy.js
+++ src/proxy.js
@@ -1,45 +1,39 @@
-const request = require('request');
-const pick = require('lodash').pick;
-const shouldCompress = require('./shouldCompress');
-const redirect = require('./redirect');
-const compress = require('./compress');
-const bypass = require('./bypass');
-const copyHeaders = require('./copyHeaders');
+const httpRequest = require('request');
+const objPick = require('lodash').pick;
+const shouldCompressImg = require('./shouldCompress');
+const redirectFunc = require('./redirect');
+const compressImg = require('./compress');
+const bypassFunc = require('./bypass');
+const headerCopy = require('./copyHeaders');
 
-function proxy(req, res) {
-  request.get(
-    req.params.url,
-    {
-      headers: {
-        ...pick(req.headers, ['cookie', 'dnt', 'referer']),
-        'user-agent': 'Bandwidth-Hero Compressor',
-        'x-forwarded-for': req.headers['x-forwarded-for'] || req.ip,
-        via: '1.1 bandwidth-hero'
-      },
-      timeout: 10000,
-      maxRedirects: 5,
-      encoding: null,
-      strictSSL: false,
-      gzip: true,
-      jar: true
-    },
-    (err, origin, buffer) => {
-      if (err || origin.statusCode >= 400) {
-        return redirect(req, res);
-      }
-
-      copyHeaders(origin, res);
-      res.setHeader('content-encoding', 'identity');
-      req.params.originType = origin.headers['content-type'] || '';
-      req.params.originSize = buffer.length;
-
-      if (shouldCompress(req)) {
-        compress(req, res, buffer);
-      } else {
-        bypass(req, res, buffer);
-      }
-    }
-  );
+function proxyRequest(req, res) {
+    httpRequest.get(req.params.url, {
+        headers: {
+            ...objPick(req.headers, ['cookie', 'dnt', 'referer']),
+            'user-agent': 'Bandwidth-Hero Compressor',
+            'x-forwarded-for': req.headers['x-forwarded-for'] || req.ip,
+            via: '1.1 bandwidth-hero'
+        },
+        timeout: 10000,
+        maxRedirects: 5,
+        encoding: null,
+        strictSSL: false,
+        gzip: true,
+        jar: true
+    }, (error, originResponse, imgBuffer) => {
+        if (error || originResponse.statusCode >= 400) {
+            return redirectFunc(req, res);
+        }
+        headerCopy(originResponse, res);
+        res.setHeader('content-encoding', 'identity');
+        req.params.originType = originResponse.headers['content-type'] || '';
+        req.params.originSize = imgBuffer.length;
+        if (shouldCompressImg(req)) {
+            compressImg(req, res, imgBuffer);
+        } else {
+            bypassFunc(req, res, imgBuffer);
+        }
+    });
 }
 
-module.exports = proxy;
+module.exports = proxyRequest;
