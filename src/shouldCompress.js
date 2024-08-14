3a557df84521ffa6f606617bd5dd592aee5e97e2
Protoman1234 <rffrgtdgd@gmail.com>
2024-08-14 18:52:28 +0530

    Update shouldCompress.js

--- src/shouldCompress.js
+++ src/shouldCompress.js
@@ -1,23 +1,21 @@
-const MIN_COMPRESS_LENGTH = 512; // Adjust the minimum compress length as desired
-const MIN_TRANSPARENT_COMPRESS_LENGTH = MIN_COMPRESS_LENGTH * 100;
+const minCompressSize = 512; 
+const transparentCompressThreshold = minCompressSize * 100;
 
-function shouldCompress(req) {
-  const { originType, originSize, webp } = req.params;
-
-  if (!originType.startsWith('image')) {
-    return false;
-  }
-  if (originSize === 0) {
-    return false;
-  }
-  if (webp && originSize < MIN_COMPRESS_LENGTH) {
-    return false;
-  }
-  if (!webp && (originType.endsWith('png') || originType.endsWith('gif')) && originSize < MIN_TRANSPARENT_COMPRESS_LENGTH) {
-    return false;
-  }
-
-  return true;
+function isCompressionNeeded(req) {
+    const { originType, originSize, webp } = req.params;
+    if (!originType.startsWith('image')) {
+        return false;
+    }
+    if (originSize === 0) {
+        return false;
+    }
+    if (webp && originSize < minCompressSize) {
+        return false;
+    }
+    if (!webp && (originType.endsWith('png') || originType.endsWith('gif')) && originSize < transparentCompressThreshold) {
+        return false;
+    }
+    return true;
 }
 
-module.exports = shouldCompress;
+module.exports = isCompressionNeeded;
