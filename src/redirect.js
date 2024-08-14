a6fc9ce2e9ff883dc2b04e827db9ac5f6fded47c
Protoman1234 <rffrgtdgd@gmail.com>
2024-08-14 18:51:58 +0530

    Update redirect.js

--- src/redirect.js
+++ src/redirect.js
@@ -1,15 +1,14 @@
-function redirect(req, res) {
-  if (res.headersSent) {
-    return;
-  }
-
-  res.setHeader('content-length', 0);
-  res.removeHeader('cache-control');
-  res.removeHeader('expires');
-  res.removeHeader('date');
-  res.removeHeader('etag');
-  res.setHeader('location', encodeURI(req.params.url));
-  res.status(302).end();
+function handleRedirect(req, res) {
+    if (res.headersSent) {
+        return;
+    }
+    res.setHeader('content-length', 0);
+    res.removeHeader('cache-control');
+    res.removeHeader('expires');
+    res.removeHeader('date');
+    res.removeHeader('etag');
+    res.setHeader('location', encodeURI(req.params.url));
+    res.status(302).end();
 }
 
-module.exports = redirect;
+module.exports = handleRedirect;
