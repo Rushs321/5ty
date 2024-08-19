957936cf6477a71053248842324110d559c6a034
Rushs321 <fyggh566@tutamail.com>
2024-08-19 15:20:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 #!/usr/bin/env node
 'use strict';
 const expressApp = require('express')();
-const authMiddleware = require('./src/authenticate');
 const paramParser = require('./src/params');
 const proxyRequest = require('./src/proxy');
 const serverPort = process.env.PORT || 8080;
 
 expressApp.enable('trust proxy');
-expressApp.get('/', authMiddleware, paramParser, proxyRequest);
+expressApp.get('/', paramParser, proxyRequest);
 expressApp.get('/favicon.ico', (req, res) => res.status(204).end());
 expressApp.listen(serverPort, () => console.log(`Listening on ${serverPort}`));
